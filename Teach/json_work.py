1ced5d22cf55e842c013feaa15c942358909acc1
lataf <alexfatkin@gmail.com>
2024-04-10 10:04:29 +0300

    move in old

--- Teach/json_work.py
+++ Teach/json_work.py
@@ -11,7 +11,6 @@ UML схема модуля
 import json
 import pandas as pd
 
-
 # https: // habr.com / ru / articles / 554274 /
 # Правила well formed JSON:
 # Данные написаны в виде пар «ключ:значение»
@@ -26,6 +25,47 @@ with open(file, 'r') as f:
 
 print('man good = ', data['man']["good"])
 print('women good = ', data['women']["good"])
+
+with open('df_data.json', 'r') as f:
+    data = json.load(f)
+
+print(data["load"])
+
+df_1 = pd.DataFrame(data['df'])  # https://habr.com/ru/companies/otus/articles/731844/
+exploded_df = df_1.explode("data", ignore_index=True)  # Чтобы развернуть столбец data
+pd.json_normalize(exploded_df['data'])  # нормализуются данные внутри столбца data
+df_2 = pd.concat([exploded_df.drop('data', axis=1),  # Убираем столбец data и заменяем его на
+                  pd.json_normalize(exploded_df['data'])], axis=1)  # нормализованные данные столбца data
+print(df_2)
+
+input_json = """
+[
+    {
+        "type": "1",
+        "name": "name 1"
+    },
+    {
+        "type": "2",
+        "name": "name 2"
+    },
+    {
+        "type": "1",
+        "name": "name 3"
+    }
+]"""
+
+# Transform json input to python objects
+input_dict = json.loads(input_json)
+
+# Filter python objects with list comprehensions
+output_dict = [x for x in input_dict if x['type'] == '1' and x['name'] == 'name 3']
+# output_dict = [x for x in input_dict if int(x['type']) > 1]
+
+# Transform python object back into json
+output_json = json.dumps(output_dict)
+
+# Show json
+print(output_json)
 
 # df = pd.DataFrame(
 #     [[46, 18], [41, 15]],
@@ -50,15 +90,3 @@ print('women good = ', data['women']["good"])
 # file = 'df_data.json'
 # # with open(file, 'w') as f:
 # #     json.dump(data, f, indent=4)
-
-with open('df_data.json', 'r') as f:
-    data = json.load(f)
-
-print(data["load"])
-
-df_1 = pd.DataFrame(data['df'])  # https://habr.com/ru/companies/otus/articles/731844/
-exploded_df = df_1.explode("data", ignore_index=True)  # Чтобы развернуть столбец data
-pd.json_normalize(exploded_df['data'])  # нормализуются данные внутри столбца data
-df_2 = pd.concat([exploded_df.drop('data', axis=1),  # Убираем столбец data и заменяем его на
-                  pd.json_normalize(exploded_df['data'])], axis=1)  # нормализованные данные столбца data
-print(df_2)
