7778dca77c5eddf7d3134b9bca60117b238a5133
lataf <alexfatkin@gmail.com>
2024-04-26 11:27:30 +0300

    rename run files

--- st_run.py
+++ st_run.py
@@ -1,6 +1,6 @@
 """
 @author: lataf 
-@file: stl_run.py 
+@file: st_run.py
 @time: 12.04.2024 17:03
 Модуль отвечает за 
 UML схема модуля
@@ -15,6 +15,6 @@ if __name__ == '__main__':
     """Запуск приложения в браузере"""
     # # Set prog_name so that the Streamlit server sees the same command line
     # # string whether streamlit is called directly or via `python -m streamlit`.
-    sys.argv = ["streamlit", "run", "stl_main.py"]
+    sys.argv = ["streamlit", "run", "st_main.py"]
     # sys.argv = ["streamlit", "run", "Teach/streamlit_work.py"]
     sys.exit(stcli.main())
